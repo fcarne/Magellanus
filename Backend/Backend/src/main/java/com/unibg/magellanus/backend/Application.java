ce6b45e1e53083941bd1342b33c8572169ff3325
fgelosa <federico.gelosa@gmail.com>
2021-12-30 18:14:22 +0100

    test

--- Backend/Backend/src/main/java/com/unibg/magellanus/backend/Application.java
+++ Backend/Backend/src/main/java/com/unibg/magellanus/backend/Application.java
@@ -8,6 +8,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+<<<<<<< Upstream, based on branch 'main' of https://github.com/fcarne/Magellanus.git
 		System.out.println("4, la vita");
+=======
+		System.out.println("");
+		System.out.println("42, il numero della vita");
+>>>>>>> 45a2edc Commit
 	}
 }
