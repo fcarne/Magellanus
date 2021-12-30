54d3c083bbcb3e83d9e9683281a408c88ebabf78
Federico Carne <federico.carne@hotmail.com>
2021-12-30 17:09:08 +0100

    Revert "Update MainActivity.kt"
    This reverts commit 832e2512328118b3901b4d33b63f4e3ddd747761.

--- App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
+++ App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
@@ -7,12 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-<<<<<<< HEAD
-        //Questo e' un commento magico v4
-=======
->>>>>>> parent of abdca93 (Metto il commento)
-=======
->>>>>>> parent of abdca93 (Metto il commento)
     }
 }
