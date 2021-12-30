f0c5404549f905024b8bec683b26984990d0cc79
Federico Carne <federico.carne@hotmail.com>
2021-12-30 17:36:13 +0100

    Test

--- App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
+++ App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
@@ -7,10 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-        //Questo e' un commento magico
-=======
->>>>>>> branch 'main' of https://github.com/fcarne/Magellanus.git
+        // silvana
     }
-    silvana
 }
