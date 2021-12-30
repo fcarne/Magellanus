5afbc68a26b04681c03bbad9f466f01716f21477
memesman12 <pota2008@live.it>
2021-12-30 16:55:59 +0100

    Metto il commentoche forse va bene mi sa di no

--- App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
+++ App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Questo e' un commento magico v4
+        //Questo e' un commento magico v5
     }
 }
