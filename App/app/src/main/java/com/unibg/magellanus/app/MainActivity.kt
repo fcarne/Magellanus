375e2e14f67257e87975a29a3e300d29ecd0b7a7
Federico Carne <federico.carne@hotmail.com>
2021-12-30 17:01:01 +0100

    Update MainActivity.kt

--- App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
+++ App/app/src/main/java/com/unibg/magellanus/app/MainActivity.kt
@@ -7,6 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< HEAD
         //Questo e' un commento magico v4
+=======
+>>>>>>> parent of abdca93 (Metto il commento)
     }
 }
